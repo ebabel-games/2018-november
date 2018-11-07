c4838ae1f8e76839481bc0f3283bad3fda21b448
Thomas Amar <hello@ebabel.eu>
2018-11-07 20:07:11 +0100

    Add resize function to scale game to viewport.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
 import 'phaser';
 
-var config = {
-  type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
+window.addEventListener('load', () => {
+  var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    backgroundColor: 0xccccff,
+    scene: {
+      preload: preload,
+      create: create
+    }
+  };
+
+  var game = new Phaser.Game(config);
+
+  function preload() {
+    this.load.image('logo', 'assets/logo.png');
   }
-};
 
-var game = new Phaser.Game(config);
+  function create() {
+    const logo = this.add.image(400, 300, 'logo');
+  }
 
-function preload() {
-  this.load.image('logo', 'assets/logo.png');
-}
+  const resizeGame = () => {
+    const canvas = document.querySelector('body > canvas');
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const windowRatio = windowWidth / windowHeight;
+    const gameRatio = game.config.width / game.config.height;
 
-function create() {
-  const logo = this.add.image(400, 300, 'logo');
-}
+    if (windowRatio < gameRatio) {
+      canvas.style.width = `${windowWidth}px`;
+      canvas.style.height = `${(windowWidth / gameRatio)}px`;
+    } else {
+      canvas.style.width = `${(windowHeight * gameRatio)}px`;
+      canvas.style.height = `${windowHeight}px`;
+    }
+  }
+
+  window.focus();
+  resizeGame();
+
+  window.addEventListener('resize', resizeGame);
+});
