0da1d3cb6b836b22f17624dc4a963b9dc9496c06
Thomas Amar <hello@ebabel.eu>
2018-11-07 22:57:00 +0100

    Add very simple loading screen.

--- src/index.js
+++ src/index.js
@@ -10,27 +10,35 @@ import * as C from './constants';
 import resizeGame from './modules/resize-game';
 import debounce from './modules/debounce';
 
+// The whole game is enclosed in an anonymous function that runs once all code is loaded.
 window.addEventListener('load', () => {
-  var config = {
+  // Phaser configuration.
+  const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
     width: 800,
     height: 600,
     backgroundColor: 0xccccff,
     scene: {
-      preload: preload,
-      create: create
+      preload,
+      create,
     }
   };
 
-  var game = new Phaser.Game(config);
+  // Phaser main game object.
+  const game = new Phaser.Game(config);
 
+  // Phaser function to preload all assets.
   function preload() {
     this.load.image('logo', 'assets/logo.png');
   }
 
+  // Phaser function to instantiate all game elements.
   function create() {
     this.add.image(400, 300, 'logo');
+
+    // Now the game is ready, so hide the loading screen.
+    document.getElementById('loading').style.display = 'none';
   }
 
   // Get focus in case the game is in an iframe.
