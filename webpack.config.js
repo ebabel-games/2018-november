263cf38a8326ca5950fc071d97a5b6bb90addca0
Thomas Amar <hello@ebabel.eu>
2018-11-07 19:46:52 +0100

    Update dependencies, license, readme. Add some styling to basic game screen. Update boilerplate package.json to match game being built.

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,27 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-
     entry: './src/index.js',
 
     output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/build/',
-        filename: 'project.bundle.js'
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/build/',
+      filename: 'project.bundle.js'
     },
 
     module: {
-        rules: [
-          {
-            test: [ /\.vert$/, /\.frag$/ ],
-            use: 'raw-loader'
-          }
-        ]
+      rules: [
+        {
+          test: [ /\.vert$/, /\.frag$/ ],
+          use: 'raw-loader'
+        }
+      ]
     },
 
     plugins: [
-        new webpack.DefinePlugin({
-            'CANVAS_RENDERER': JSON.stringify(true),
-            'WEBGL_RENDERER': JSON.stringify(true)
-        })
+      new webpack.DefinePlugin({
+        'CANVAS_RENDERER': JSON.stringify(true),
+        'WEBGL_RENDERER': JSON.stringify(true)
+      })
     ]
-
 };
