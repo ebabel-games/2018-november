2c3e4782650c33006b80a29649791910bda48dcf
Thomas Amar <hello@ebabel.eu>
2018-11-17 03:48:53 +0100

    Offline setup

--- src/utils/pwa.js
+++ src/utils/pwa.js
@@ -1,6 +1,8 @@
+// Progressive Web App (PWA) setup.
+// Note: also see file /service-worker.js, which should be a separate single file at the root of the website.
 const pwa = () => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
       .then((registration) => {
         console.log(registration);
       })
